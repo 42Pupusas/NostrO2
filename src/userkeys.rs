3d60f706b18168e2e6bdca419ff752dced8d39b8
Joya, Edgar <eujc21@gmail.com>
2023-08-02 13:20:46 -0400

    Created a note, and notated the next test[4~

--- src/userkeys.rs
+++ src/userkeys.rs
@@ -14,7 +14,6 @@ impl UserKeys {
           &hex::decode(private_key).unwrap()
         ).unwrap();
         let keypair = KeyPair::from_secret_key(&secp, &secret_key);
-        let public_key = PublicKey::from_secret_key(&secp, &secret_key);
         UserKeys { keypair }
     }
 
@@ -22,30 +21,37 @@ impl UserKeys {
         return self.keypair.public_key().to_string()[2..].to_string();
     }
 
-    pub fn sign_nostr_event(&self, event: Note) -> SignedNote {
+    pub fn sign_nostr_event(&self, note: Note) -> SignedNote {
         // Serialize the event as JSON
-        let json_str = event.serialize_for_nostr();
+        let json_str = note.serialize_for_nostr();
+        
         // Compute the SHA256 hash of the serialized JSON string
         let mut hasher = Sha256::new();
         hasher.update(json_str);
+        
+        // Hex Encod the hash
         let hash_result = hasher.finalize();
         let id = hex::encode(hash_result);
+        
+        // Create a byte representation of the hash.
         let secp = Secp256k1::new();
-
         let id_message = Message::from_slice(&hash_result).unwrap();
         
+        // Sign it with the schnorr.
         let sig = secp
             .sign_schnorr_no_aux_rand(&id_message, &self.keypair)
             .to_string();
-        let signed_event = SignedNote {
+        
+        // Crteate the signed
+        let signed_note = SignedNote {
             id,
             pubkey: self.get_public_key(),
-            created_at: event.created_at,
-            kind: event.kind,
-            tags: event.tags,
-            content: event.content,
+            created_at: note.created_at,
+            kind: note.kind,
+            tags: note.tags,
+            content: note.content,
             sig,
         };
-        signed_event
+        signed_note
     }
 }
--- tests/main.rs
+++ tests/main.rs
@@ -1,6 +1,8 @@
 extern crate rustr;
 use rustr::userkeys::{UserKeys};
 use rustr::nostr::{Note, SignedNote};
+use secp256k1::schnorr::{Signature};
+use secp256k1::{Message, XOnlyPublicKey};
 
 const pk1: &str = "07947aa9d48d099604ea53e2d347203d90fb133d77a430de43373b8eabd6275d";
 const pk2: &str = "4f6ddf3e79731d1b7039e28feb394e41e9117c93e383d31e8b88719095c6b17d";
@@ -22,23 +24,35 @@ mod tests {
     assert_ne!("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",uk.get_public_key());
   }
 
-  // Sign messages
+  // Created and verified the signature of a note.
   #[test]
   fn test_create_note() {
-    let content = "- .... .. ... / .. ... / .- / -- . ... ... .- --. .";
-    let uk = UserKeys::new(pk1);
-    let note = Note::new(
-      uk.get_public_key().to_string(),
+    let content_of_note = "- .... .. ... / .. ... / .- / -- . ... ... .- --. .";
+    let user_key_pair = UserKeys::new(pk1);
+    let unsigned_note = Note::new(
+      user_key_pair.get_public_key().to_string(),
       [].to_vec(),
       300,
-      content.to_string()
+      content_of_note.to_string()
     );
-    let en_test = NoteTest::new(
-      uk.sign_nostr_event(note)
-    )
-    // Should be able to verify the signature against
-    // the id the and the public key.
+    let signed_note = user_key_pair.sign_nostr_event(unsigned_note);
+    let signature_of_signed_note = Signature::from_slice(
+      &hex::decode(signed_note.sig).unwrap()
+    ).unwrap();
+    let message_of_signed_note = Message::from_slice(
+      &hex::decode(signed_note.id).unwrap()
+    ).unwrap();
+    let public_key_of_signed_note = XOnlyPublicKey::from_slice(
+      &hex::decode(signed_note.pubkey).unwrap()
+    ).unwrap();
+    signature_of_signed_note.verify(
+      &message_of_signed_note,
+      &public_key_of_signed_note
+    );
   }
-  // Send messages to a relay.
-  // Recieve messages to a relay.
+  // Send a kind 1 message to relay and recieve it.
+  // Open relay to url
+  // Create a kind 1 subscription
+  // Send a kind 1 message
+  // Loop over recieving message to retrieve a kind 1
 }
