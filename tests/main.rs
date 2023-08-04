95db06bda28792b784ce318f333ef2ad0f5855c1
Joya, Edgar <eujc21@gmail.com>
2023-08-04 13:19:40 -0400

    Cleaning up structs and implementations, pt2

--- tests/main.rs
+++ tests/main.rs
@@ -1,11 +1,9 @@
 extern crate nostro2;
 use nostro2::userkeys::{UserKeys};
 use nostro2::notes::{Note, SignedNote};
-use secp256k1::schnorr::{Signature};
-use secp256k1::{Message, XOnlyPublicKey};
 
-const pk1: &str = "07947aa9d48d099604ea53e2d347203d90fb133d77a430de43373b8eabd6275d";
-const pk2: &str = "4f6ddf3e79731d1b7039e28feb394e41e9117c93e383d31e8b88719095c6b17d";
+const PK1: &str = "07947aa9d48d099604ea53e2d347203d90fb133d77a430de43373b8eabd6275d";
+const PK2: &str = "4f6ddf3e79731d1b7039e28feb394e41e9117c93e383d31e8b88719095c6b17d";
 
 #[cfg(test)]
 mod tests {
@@ -14,13 +12,13 @@ mod tests {
   // Test Private Public UserKeys Match
   #[test]
   fn test_user_key() {
-    let uk = UserKeys::new(pk1);
-    assert_eq!(pk2,uk.get_public_key());
+    let uk = UserKeys::new(PK1);
+    assert_eq!(PK2,uk.get_public_key());
   }
   // Test Private Public UserKeys Do Not Match
   #[test]
   fn test_not_user_key() {
-    let uk = UserKeys::new(pk1);
+    let uk = UserKeys::new(PK1);
     assert_ne!("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",uk.get_public_key());
   }
 
@@ -28,7 +26,7 @@ mod tests {
   #[test]
   fn test_create_note() {
     let content_of_note = "- .... .. ... / .. ... / .- / -- . ... ... .- --. .";
-    let user_key_pair = UserKeys::new(pk1);
+    let user_key_pair = UserKeys::new(PK1);
     let unsigned_note = Note::new(
       user_key_pair.get_public_key().to_string(),
       [].to_vec(),
